b488e676f3fc231291e17c21b5ee8e20e4861567
anandyadav1 <ay4799958@gmail.com>
2025-07-15 01:42:12 +0530

    Problem on heaps

--- Heaps/findByRank.java
+++ Heaps/findByRank.java
@@ -18,7 +18,7 @@ public class findByRank {
     public static void main(String[] args) {
         int arr[]={2,3,5,1,7,4,56,344,778,346,45,22};
         String b[]={"Adarsh","Arun", "Aman","Anand","andgf","uuqwujf", "hdughaus", "ajdbhy","qwytti","sjadkjhdf","adsgjh","ahdsgahsg"};
-        PriorityQueue<Student> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Student> pq = new PriorityQueue<>((a,c)->c.rank-a.rank);
         for(int i=0; i<arr.length; i++){
             pq.add(new Student(b[i], arr[i]));
         }
