faa5635c71a430ee80bf13ef1a2c3705f06be836
anandyadav1 <ay4799958@gmail.com>
2025-08-13 05:47:43 +0530

    Find largest String in lexicografical order

--- String/largestString.java
+++ String/largestString.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class largestString {
     public static void compare(String str[]){
@@ -11,7 +11,15 @@ public class largestString {
         System.out.println(largest);
     }
     public static void main(String[] args) {
-        String fruits []= {"apple", "mango", "banana", "sdjfjghjs", "aaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
+        String fruits []= {"apple", "mango", "banana", "sdjfjghjs", "t"};
+        Integer a[]={4,5,6,7,8};
+        int b[]=new int[a.length];
+        for(int i=0; i<a.length; i++) b[i]=a[i];
+        Arrays.sort(fruits, Collections.reverseOrder());
+        Arrays.sort(a, Collections.reverseOrder());
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+        System.out.println(Arrays.toString(fruits));
         compare(fruits);
     }
 }
